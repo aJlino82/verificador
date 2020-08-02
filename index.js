d652734669bfa8ae0b23d6980df72c5a903d5e62
Adailton Junior <53987087+aJlino82@users.noreply.github.com>
2020-08-02 12:32:27 -0300

    Mudanças no Dokerfile
    Domingo 12:32

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const  express = require('express');
 
+const express = require('express');
 const app = express();
-
+const router = express.Router();
 const path = require('path');
 
-const router = express.Router();
+const PORT = 3000
+const HOST = '0.0.0.0'
 
 
-
-router.get('/',function(req, res) {
+router.get('/',function(req, res,) {
     res.sendFile(path.join(__dirname+'/index.html'));
-    
 })
-
 
 app.use('/',router);
 
-app.listen(process.env.port || 3000);
+
+app.listen(PORT, HOST);
+console.log('Server rodando!')
